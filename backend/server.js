c256e1edf403049998908e024fb31f9547143d13
krishanu009 <krishanudeb58@gmail.com>
2024-05-16 01:23:44 +0530

    hhh

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,11 @@ const port = process.env.PORT || 5000;
 
 const server = require('http').createServer(app);
 const io = require("socket.io")(server);
-server.listen(3000, function() {
+server.listen(process.env.PORT || 5000, function() {
   var host = server.address().address
   var port = server.address().port
   console.log('App listening at https://%s:%s', host, port)
-});;
+});
 console.log("socket",io);
 
 // server.listen(process.env.PORT || 3001, () => {
