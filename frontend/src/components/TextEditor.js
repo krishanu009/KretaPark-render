1e3c80519708d3274cd8d7761818879460eb438a
krishanu009 <krishanudeb58@gmail.com>
2024-05-16 01:14:04 +0530

    sicket

--- frontend/src/components/TextEditor.js
+++ frontend/src/components/TextEditor.js
@@ -34,7 +34,15 @@ function TextEditor({scripId,title,setTitle,userInfo}) {
   }, []); 
   useEffect(() => {
     // const s = io("http://localhost:3001");
-    const s = io('https://kretapark.onrender.com');
+    const s = io('https://kretapark.onrender.com',{
+      reconnectionDelay: 1000,
+      reconnection: true,
+      reconnectionAttemps: 10,
+      transports: ['websocket'],
+      agent: false,
+      upgrade: false,
+      rejectUnauthorized: false
+  });
     setSocket(s);
     console.log("socket", s);
     return () => {
